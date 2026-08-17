d300f025b75d11933da3219c3db643af583e2584
ngurahariwhrihaspati <ari.microsoft.hp@gmail.com>
2026-08-17 17:15:00 +0800

    completed project uploaded exclude sign in options

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -141,10 +141,90 @@ exports.author_delete_post = async (req, res, next) => {
 
 // Display Author update form on GET.
 exports.author_update_get = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+  // Get details of author and all their books (in parallel)
+  const [author, allBooksbyAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    // No results.
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("author_form", {
+    title: "Update Author",
+    author,
+    author_books: allBooksbyAuthor,
+  });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .trim()
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .trim()
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization
+  async (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data
+    const author_update = {
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    };
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      const allBooksbyAuthor = await Book.find(
+        { author: req.params.id },
+        "title summary",
+      ).exec();
+
+      res.render("author_form", {
+        title: "Update Author",
+        author: author_update,
+        author_books: allBooksbyAuthor,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    const updatedAuthor = await Author.findByIdAndUpdate(
+      req.params.id,
+      author_update,
+      {},
+    );
+    // Redirect to author detail page.
+    res.redirect(updatedAuthor.url);
+  },
+];
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -104,7 +104,7 @@ exports.genre_delete_get = async (req, res, next) => {
 exports.genre_delete_post = async (req, res, next) => {
   const [genre, allBooksbyGenre] = await Promise.all([
     Genre.findById(req.params.id).exec(),
-    Book.find({ book: req.params.id }, "title summary").exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
   ]);
 
   if (genre === null) {
@@ -121,10 +121,82 @@ exports.genre_delete_post = async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (genre === null) {
+    // No results.
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_form", {
+    title: "Update Genre",
+    genre,
+    genre_books: booksInGenre,
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and sanitize fields.
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization
+  async (req, res, next) => {
+    //Extract thevalidation errors from a request.
+    const errors = validationResult(req);
+
+    //Create a genre object with escaped and trimmed data.
+    const genre_update = { name: req.body.name };
+
+    if (!errors.isEmpty()) {
+      // Get all books and genres for form
+      const [genre, booksInGenre] = await Promise.all([
+        Genre.findById(req.params.id).exec(),
+        Book.find({ genre: req.params.id }, "title summary").exec(),
+      ]);
+
+      if (genre === null) {
+        //No results
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre,
+        genre_books: booksInGenre,
+      });
+      return;
+    }
+
+    // Data from form is valid.
+    // Check if Genre with same name already exists.
+
+    const genreExists = await Genre.findOne({ name: req.body.name })
+      .collation({ locale: "en", strength: 2 })
+      .exec();
+    if (genreExists) {
+      // Genre exists, redirect to its detail page.
+      res.redirect(genreExists.url);
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    const updatedGenre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      genre_update,
+      {},
+    );
+    // Redirect to book detail page.
+    res.redirect(updatedGenre.url);
+  },
+];
